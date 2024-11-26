9e469b4c0371f29e8b1887512110c8b9128b7c90
Jose Garda Seta <jose8gard@gmail.com>
2024-11-27 02:13:36 +0700

    [FIX] config gatsby

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,23 +1,13 @@
-module.exports = {
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
   siteMetadata: {
-    title: `The Wedding of Tiffany & Jared`,
-    description: `Website untuk pernikahan Tiffany & Jared`,
-    author: `@invitato`,
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Wedding Website`,
-        short_name: `Wedding`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#000000`,
-        display: `standalone`, // Pilih 'standalone' atau 'minimal-ui'
-        icon: `src/images/favicon.png`, // Ganti dengan path ke favicon Anda
-        // Anda bisa menambahkan konfigurasi lebih lanjut untuk PWA seperti splash screens, dll
-      },
-    },
-    // Plugin lain...
-  ],
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
 };
+
+export default config;
